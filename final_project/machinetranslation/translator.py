817b8a14001036033d3e026990744fefb0fa70ad
jaogye <jao_ec@yahoo.com>
2023-03-30 06:56:50 -0400

    Modification on key leaking

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,10 +1,13 @@
 import os
 import json
+from dotenv import load_dotenv
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-apikey="g0u51-XDIGp1Cq150Ln-D75ha5QPAtvpD1r1GURT0ykO"
-url="https://api.us-south.language-translator.watson.cloud.ibm.com/instances/95535a59-0052-47c3-8075-fe0e2eab862c"
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
 
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
